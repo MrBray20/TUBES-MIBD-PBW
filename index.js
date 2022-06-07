88c181ec68e878caf5e06af07ffc773e630168fe
MrBray20 <brianmone2@gmail.com>
2022-06-07 14:00:01 +0800

    mengganti router dan file lainnya

--- index.js
+++ index.js
@@ -14,60 +14,41 @@ app.get('/',async (req,res)=>{
     res.render('login');
 });
 
-app.post('/homeadmin',async (req,res)=>{
-    res.redirect('homeadmin');
+app.post('/home',async (req,res)=>{
+    res.redirect('home');
 });
 
-app.get('/homeadmin',async (req,res)=>{
-    res.render('homeadmin');
+app.get('/home',async (req,res)=>{
+    res.render('home');
 });
 
 app.get('/logout',async (req,res)=>{
     res.render('login');
 });
 
-app.get('/bandungbarat',async (req,res)=>{
-    res.render('adminkecamatan');
-});
-app.get('/cisarua',async (req,res)=>{
-    res.render('adminkelurahan');
-});
 app.get('/kecamatan',async (req,res)=>{
-    res.render('adminkecamatandata');
-});
-app.get('/kelurahan',async (req,res)=>{
-    res.render('adminkelurahandata');
-});
-
-app.get('/rw',async (req,res)=>{
-    res.render('adminrw');
-});
-app.get('/users',async (req,res)=>{
-    res.render('users');
-});
-app.get('/produk',async (req,res)=>{
-    res.render('produk');
-});
-app.get('/perioda',async (req,res)=>{
-    res.render('perioda');
-});
-app.get('/addkecamatan',async (req,res)=>{
-    res.render('addkecamatan');
-});
-app.get('/adminkecamatandata',async (req,res)=>{
-    res.redirect('adminkecamatan');
-});
-app.post('/adminkecamatandata',async (req,res)=>{
-    res.redirect('adminkecamatan');
+    res.render('kecamatan');
 });
 app.post('/kecamatan',async (req,res)=>{
     res.redirect('kecamatan');
 });
-app.get('/addkelurahan',async (req,res)=>{
-    res.render('addkelurahan');
+app.get('/addkecamatan',async (req,res)=>{
+    res.render('addkecamatan');
+});
+
+app.get('/kelurahan',async (req,res)=>{
+    res.render('kelurahan');
 });
 app.post('/kelurahan',async (req,res)=>{
     res.redirect('kelurahan');
+});
+app.get('/addkelurahan',async (req,res)=>{
+    res.render('addkelurahan');
+});
+
+
+app.get('/rw',async (req,res)=>{
+    res.render('rw');
 });
 app.get('/addrw',async (req,res)=>{
     res.render('addrw');
@@ -75,6 +56,41 @@ app.get('/addrw',async (req,res)=>{
 app.post('/addrw',async (req,res)=>{
     res.redirect('rw');
 });
+
+
+app.get('/users',async (req,res)=>{
+    res.render('users');
+});
+app.get('/adduser',async (req,res)=>{
+    res.render('adduser');
+});
+app.post('/adduser',async (req,res)=>{
+    res.redirect('users');
+});
+
+
+app.get('/produk',async (req,res)=>{
+    res.render('produk');
+});
+app.get('/addproduk',async (req,res)=>{
+    res.render('addproduk');
+});
+app.post('/addproduk',async (req,res)=>{
+    res.redirect('produk');
+});
+
+
+app.get('/perioda',async (req,res)=>{
+    res.render('perioda');
+});
+app.get('/addperioda',async (req,res)=>{
+    res.render('addperioda');
+});
+app.post('/addperioda',async (req,res)=>{
+    res.redirect('perioda');
+});
+
+
 app.listen(port,()=>{
     console.log('ready!');
 });
