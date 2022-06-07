e1a2d0c2588ca7a19acccaa1c99059a34d4d3573
ReygaElkigia <106369845+ReygaElkigia@users.noreply.github.com>
2022-06-07 22:51:00 +0700

    menambah page staf penjualan + create folder page + path index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import * as url from 'url';
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import express from 'express';
 import path from 'path';
 import mysql from 'mysql';
@@ -7,6 +10,9 @@ const port = 8080;
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', [path.join(__dirname, 'views'), 
+path.join(__dirname, 'views/Admin'),
+path.join(__dirname, 'views/StafPenjualan')]);
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.resolve('public')));
 
@@ -49,9 +55,9 @@ app.post('/home',async (req,res)=>{
 
     res.redirect('home');
 });
-
-app.get('/home',async (req,res)=>{
-    res.render('home');
+//Admin Page
+app.get('/homeadmin',async (req,res)=>{
+    res.render('homeadmin');
 });
 
 app.get('/logout',async (req,res)=>{
@@ -127,6 +133,30 @@ app.listen(port,()=>{
     console.log('ready!');
 });
 
+//StafPenjualanPage
+app.get('/',async (req,res)=>{
+    res.render('login');
+});
+app.get('/homestafpenjualan',async (req,res)=>{
+    res.render('homestafpenjualan');
+});
+app.post('/homestaf',async (req,res)=>{
+    res.redirect('homestaf');
+});
+
+app.get('/homestaf',async (req,res)=>{
+    res.render('homestaf');
+});
+
+app.get('/logout',async (req,res)=>{
+    res.render('login');
+});
+app.get('/kecamatan',async (req,res)=>{
+    res.render('stafperiodepesanan');
+});
+app.get('/kelurahan',async (req,res)=>{
+    res.render('stafstatuspembayaran');
+});
 
 var sec = 'admin123'
 var hash = crypto.createHash('sha256').update(sec).digest('base64')
