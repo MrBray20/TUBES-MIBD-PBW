1e2f074431169c33e4825520a74af18578001bd6
MrBray20 <brianmone2@gmail.com>
2022-06-08 01:13:56 +0800

    login admin cookies sudah bisa

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import path from 'path';
 import mysql from 'mysql';
 import crypto from 'crypto';
+import multer from 'multer';
+import session from 'express-session';
+import cookiePerser from 'cookie-parser'
 
 const port = 8080;
 const app = express();
-
+app.use(cookiePerser());
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.resolve('public')));
@@ -17,6 +20,11 @@ const pool = mysql.createPool({
     host:'localhost',
     connectionLimit:10
 });
+
+const generateAuthToken = () =>{
+    return crypto.randomBytes(30).toString('hex');
+}
+const authTokens = {};
 
 const dbConnect = () =>{
     return new Promise((resolve,rejects)=>{
@@ -31,9 +39,16 @@ const dbConnect = () =>{
     })
 }
 
-const getuser = (conn,username,passs) =>{
+const getuser = (conn,username,pass) =>{
     return new Promise((resolve, rejects) =>{
-        conn.query(`SELECT username, password`)
+        conn.query(`SELECT username, pass, role_u FROM users WHERE username LIKE '${username}' AND pass LIKE '${pass}'`,(err,result)=>{
+            if(err){
+                rejects(err);
+            }
+            else{
+                resolve(result);
+            }
+        })
     })
 }
 
@@ -43,83 +58,189 @@ app.get('/',async (req,res)=>{
 
 app.post('/home',async (req,res)=>{
     const{username ,password} = req.body
-
+    const conn = await dbConnect();
     const hashpass = crypto.createHash('sha256').update(password).digest('base64')
-    console.log(hashpass)
+    const user = await getuser(conn,username,hashpass);
+    conn.release();
+    if(user.length>0){
+        const authToken = generateAuthToken();
+        authTokens[authToken]=user
+        res.cookie('AuthToken',authToken);
+        if (user[0].role_u==='admin') {
+            res.redirect('homeadmin')
+        }
+        else{
+            res.redirect('/');
+        }
+    }
+    else{
+        console.log('tidak ad')
+    }
 
-    res.redirect('home');
+    
 });
 
-app.get('/home',async (req,res)=>{
-    res.render('home');
+app.use((req,res,next) =>{
+    const authToken = req.cookies['AuthToken']
+    req.user = authTokens[authToken];
+    next();
+})
+
+app.get('/homeadmin',async (req,res)=>{
+    if(req.user){
+        res.render('homeadmin');
+    }else{
+        res.redirect('/');
+    }
 });
 
 app.get('/logout',async (req,res)=>{
-    res.render('login');
+    const cookie = req.cookies;
+    for (var i in cookie) {
+        if (!cookie.hasOwnProperty(i)) {
+            continue;
+        }
+        res.cookie(i, '',{expires: new Date(0)});
+    }
+    res.redirect('/');
+
 });
 
 app.get('/kecamatan',async (req,res)=>{
-    res.render('kecamatan');
+    if(req.user){
+        res.render('kecamatan');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/kecamatan',async (req,res)=>{
-    res.redirect('kecamatan');
+    if(req.user){
+        res.redirect('kecamatan');
+    }else{
+        res.redirect('/');
+    }
 });
+
 app.get('/addkecamatan',async (req,res)=>{
-    res.render('addkecamatan');
+    if(req.user){
+        res.render('addkecamatan');
+    }else{
+        res.redirect('/');
+    }
 });
 
 app.get('/kelurahan',async (req,res)=>{
-    res.render('kelurahan');
+    if(req.user){
+        res.render('kelurahan');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/kelurahan',async (req,res)=>{
-    res.redirect('kelurahan');
+    if(req.user){
+        res.redirect('kelurahan');
+    }else{
+        res.redirect('/');
+    }
 });
 app.get('/addkelurahan',async (req,res)=>{
-    res.render('addkelurahan');
+    if(req.user){
+        res.render('addkelurahan');
+    }else{
+        res.redirect('/');
+    }
 });
 
 
 app.get('/rw',async (req,res)=>{
-    res.render('rw');
+    if(req.user){
+        res.render('rw');
+    }else{
+        res.redirect('/');
+    }
 });
 app.get('/addrw',async (req,res)=>{
-    res.render('addrw');
+    if(req.user){
+        res.render('addrw');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/addrw',async (req,res)=>{
-    res.redirect('rw');
+    if(req.user){
+        res.redirect('rw');
+    }else{
+        res.redirect('/');
+    }
 });
 
 
 app.get('/users',async (req,res)=>{
-    res.render('users');
+    if(req.user){
+        res.render('users');
+    }else{
+        res.redirect('/');
+    }
 });
 app.get('/adduser',async (req,res)=>{
-    res.render('adduser');
+    if(req.user){
+        res.render('adduser');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/adduser',async (req,res)=>{
-    res.redirect('users');
+    if(req.user){
+        res.redirect('users');
+    }else{
+        res.redirect('/');
+    }
 });
 
 
 app.get('/produk',async (req,res)=>{
-    res.render('produk');
+    if(req.user){
+        res.render('produk');
+    }else{
+        res.redirect('/');
+    }
 });
 app.get('/addproduk',async (req,res)=>{
-    res.render('addproduk');
+    if(req.user){
+        res.render('addproduk');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/addproduk',async (req,res)=>{
-    res.redirect('produk');
+    if(req.user){
+        res.redirect('produk');
+    }else{
+        res.redirect('/');
+    }
 });
 
 
 app.get('/perioda',async (req,res)=>{
-    res.render('perioda');
+    if(req.user){
+        res.render('perioda');
+    }else{
+        res.redirect('/');
+    }
 });
 app.get('/addperioda',async (req,res)=>{
-    res.render('addperioda');
+    if(req.user){
+        res.render('addperioda');
+    }else{
+        res.redirect('/');
+    }
 });
 app.post('/addperioda',async (req,res)=>{
-    res.redirect('perioda');
+    if(req.user){
+        res.redirect('perioda');
+    }else{
+        res.redirect('/');
+    }
 });
 
 
@@ -128,6 +249,6 @@ app.listen(port,()=>{
 });
 
 
-var sec = 'admin123'
-var hash = crypto.createHash('sha256').update(sec).digest('base64')
-console.log(hash);
+// var sec = 'admin123'
+// var hash = crypto.createHash('sha256').update(sec).digest('base64')
+// console.log(hash);
