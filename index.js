4187e3737ba72fedca053b7c2e7e0f0fb4e2bede
ReygaElkigia <106369845+ReygaElkigia@users.noreply.github.com>
2022-06-25 20:15:18 +0700

    fitur ganti profile (blm connect db)

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ const pool = mysql.createPool({
     database:'tubesmibdpbw',
     host:'localhost',
     dateStrings: true,
-    connectionLimit:10,
-    port: 8112
+    connectionLimit:10
+    //port: 8112
 });
 
 const generateAuthToken = () =>{
